9ebfe84ae71d64fa060f8371394cebee9b64ed4d
Ben Hughes <benhughes@shaw.ca>
2015-11-03 22:16:19 -0800

    Implemented weak fox ai.
    Foxes move toward closest visible rabbit, or stay still. Foxes will eat
    any rabbit that is close enough.

--- src/ca/ubc/ece/cpen221/mp4/ai/FoxAI.java
+++ src/ca/ubc/ece/cpen221/mp4/ai/FoxAI.java
@@ -20,17 +20,48 @@ import ca.ubc.ece.cpen221.mp4.items.animals.*;
  * Your Fox AI.
  */
 public class FoxAI extends AbstractAI {
-	private int closest = 2; // max number; greater than fox's view range
+    private int  closest               = 2;                // max number;
+                                                           // greater than fox's
+                                                           // view range
+    private int  closestRabbitDistance = Integer.MAX_VALUE;
+    private Item closestRabbit         = null;
 
-	public FoxAI() {
+    public FoxAI() {
 
-	}
+    }
 
-	@Override
-	public Command getNextAction(ArenaWorld world, ArenaAnimal animal) {
-		// TODO: Change this. Implement your own AI to make decisions regarding
-		// the next action.
-		return new WaitCommand();
-	}
+    @Override
+    public Command getNextAction(ArenaWorld world, ArenaAnimal animal) {
+        Set<Item> nearbyItems = world.searchSurroundings(animal);
+        closestRabbitDistance = Integer.MAX_VALUE;
+        
+        Command action = new WaitCommand();;
+        
+        for (Item item : nearbyItems) {
+            if (item.getName() == "Rabbit") {
+                int distance = item.getLocation()
+                        .getDistance(animal.getLocation());
+                if (distance == 1) {
+                    return new EatCommand(animal, item);
+                }
+                if (distance <= closestRabbitDistance) {
+                    closestRabbit = item;
+                    closestRabbitDistance = distance;
+                    Direction dirTo = Util.getDirectionTowards(
+                            animal.getLocation(), item.getLocation());
+                    Location idealLocation = new Location(animal.getLocation(),
+                            dirTo);
+                    if (Util.isLocationEmpty((World) world, idealLocation)) {
+                        action = new MoveCommand(animal, idealLocation);
+                    } else {
+                        action = new MoveCommand(animal,
+                                Util.getRandomEmptyAdjacentLocation(
+                                        (World) world, animal.getLocation()));
+                    }
+                }
+            }
+        }
+        return action;
+    }
 
 }
